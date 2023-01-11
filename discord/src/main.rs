30a699fbd43861d12da56780ba9af7396f06d885
Matthew Bryant <matt23488@gmail.com>
2023-01-11 13:57:06 -0500

    removed dead code in discord

--- discord/src/main.rs
+++ discord/src/main.rs
@@ -1,17 +1,14 @@
-// use std::collections::HashMap;
 use std::sync::mpsc;
 use std::sync::Arc;
 use std::sync::Mutex;
 
-// use config::Config;
 use config::Environment;
 use game_server::Dispatcher;
 use serde::Deserialize;
 use serde::Serialize;
 use serenity::async_trait;
-use serenity::framework::standard::macros::{command, group};
-use serenity::framework::standard::{CommandResult, StandardFramework};
-use serenity::model::channel::Message;
+use serenity::framework::standard::macros::group;
+use serenity::framework::standard::StandardFramework;
 use serenity::model::prelude::ChannelId;
 use serenity::prelude::*;
 
@@ -24,7 +21,6 @@ use ws_protocol::BjornWsClientType;
 use minecraft::serenity::*;
 
 #[group]
-#[commands(ping)]
 #[commands(start, stop, save, say, tp)] // TODO: Macro to take in `General` and add this macro
 struct General;
 
@@ -41,7 +37,7 @@ impl EventHandler for Handler {}
 
 #[tokio::main]
 async fn main() {
-    let ws_client = std::sync::Arc::new(std::sync::Mutex::new(Some(BjornWsClient::new(
+    let ws_client = Arc::new(Mutex::new(Some(BjornWsClient::new(
         BjornWsClientType::Discord,
     ))));
 
@@ -87,10 +83,7 @@ async fn main() {
     let sender = Mutex::new(Some(Dispatcher::new(sender)));
 
     let mut data = discord_client.data.write().await;
-    // let mut map = HashMap::new();
-    // map.insert("ws".into(), ws_client);
-    // data.insert::<BjornWsClient>(map);
-    // data.insert::<Dispatcher>(map);
+    
     data.insert::<Dispatcher>(sender);
     drop(data);
 
@@ -113,40 +106,3 @@ async fn main() {
         println!("An error occurred while running the client: {:?}", why);
     }
 }
-
-#[command]
-async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "Pong!").await?;
-
-    Ok(())
-}
-
-// #[command]
-// async fn ws(ctx: &Context, msg: &Message) -> CommandResult {
-//     let data = ctx.data.read().await;
-
-//     if msg.content.len() < 4 {
-//         msg.reply(ctx, "You must specify a message to send.")
-//             .await?;
-//     } else {
-//         let ws_closed = if let Some(ws) = data
-//             .get::<BjornWsClient>()
-//             .unwrap()
-//             .get("ws")
-//             .unwrap()
-//             .lock()
-//             .unwrap()
-//             .as_ref()
-//         {
-//             ws.send_message(&msg.content[4..]).is_err()
-//         } else {
-//             true
-//         };
-
-//         if ws_closed {
-//             msg.reply(ctx, "WS connection closed.").await?;
-//         }
-//     }
-
-//     Ok(())
-// }
