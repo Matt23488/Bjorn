670820db869bbb5e67cd7717ac93791e52f11716
Matthew Bryant <matt23488@gmail.com>
2025-02-07 09:56:40 -0500

    Forgot to register the command with Serenity

--- minecraft/src/serenity/config.rs
+++ minecraft/src/serenity/config.rs
@@ -14,7 +14,7 @@ impl TypeMapKey for DiscordConfig {
 }
 
 #[group]
-#[commands(mstart, mstop, save, tp, players, mplayer, messages, cmd)] // TODO: Macro to add commands in?
+#[commands(mstart, mstop, save, tp, players, mplayer, messages, backup, cmd)] // TODO: Macro to add commands in?
 struct Minecraft;
 
 pub struct MessageHandler;
